1299d115ba52e3141e0db40d2dec70adec8f9018
Misty <2584570617@qq.com>
2018-12-19 09:33:34 +0800

    code file

---
+++ alien_invasion_game/Settings.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+# __author__ = wmm
+
+class Settings():
+    """存储《外星人入侵》的所有类"""
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
